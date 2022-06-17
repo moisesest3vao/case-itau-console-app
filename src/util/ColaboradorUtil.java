6277838e5ef15f3f58d5d17601b6880f61a68896
Moisés Estevão <moises.silva@soluarq.com.br>
2022-06-17 20:47:47 -0300

    refactor

--- src/util/ColaboradorUtil.java
+++ src/util/ColaboradorUtil.java
@@ -5,6 +5,7 @@ import model.Colaborador;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 public class ColaboradorUtil {
     private static List<String> historicoLogins = new ArrayList<>();
@@ -27,36 +28,44 @@ public class ColaboradorUtil {
         return login;
     }
 
-    private boolean verificaRepeticao(String identificador) {
-        return historicoLogins.stream().anyMatch( login -> {
-            return Objects.equals(login, identificador);
-        });
+    private String montaLogin(String[] nomes){
+        String parte1 = null;
+        String parte2 = null;
+        int nomesCount = nomes.length;
+        boolean validaLogin = true;
+        int contador = 0;
+
+        while(validaLogin){
+            if(contador > 3000){
+                throw new RuntimeException("Não é possível gerar um usuário dinamicamente com este nome");
+            }
+
+            Random random = new Random();
+            int aux = random.nextInt(((nomesCount - 1)) + 1);
+            int aux2 = random.nextInt(((nomesCount - 1)) + 1);
+
+            if(nomes[aux].length() >=4){
+                parte1 = nomes[aux].substring(0,4);
+                if(nomes[aux2].length() >= 3){
+                    parte2 = nomes[aux2].substring(0,3);
+                }
+            }
+            String login = parte1+parte2;
+            validaLogin = this.verificaRepeticaoOuIgualdade(login);
+
+            contador++;
+        }
+
+        return parte1+parte2;
     }
 
     private String removeElementosDeLigacao(String nome) {
         return nome.toUpperCase().replaceAll(expressaoRegular, "$1 $5");
     }
 
-    private String montaLogin(String[] nomes){
-        String parte1 = null;
-        String parte2 = null;
-
-        if(nomes[0].length() >=4){
-            parte1 = nomes[0].substring(0,4);
-            if(nomes[1].length() >= 3){
-                parte2 = nomes[1].substring(0,3);
-            }
-        }
-
-        if(this.verificaRepeticao(parte1+parte2)){
-            if(nomes[1].length() >=4){
-                parte1 = nomes[1].substring(0,4);
-                if(nomes[0].length() >= 3){
-                    parte2 = nomes[0].substring(0,3);
-                }
-            }
-        }
-
-        return parte1+parte2;
+    private boolean verificaRepeticaoOuIgualdade(String identificador) {
+        return historicoLogins.stream().anyMatch( login -> {
+            return Objects.equals(login, identificador) || identificador.substring(0, 3).equals(identificador.substring(4, 7));
+        });
     }
 }
